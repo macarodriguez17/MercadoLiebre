e36f910929e48c3964ea04df60fe0fce8f4aede9
macarodriguez17 <macarenarodriguezcaniete@gmail.com>
2022-04-13 19:59:28 -0300

    Cambios para poder usar heroku

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.listen(3300,()=>{
+app.listen(process.env.PORT || 3300,()=>{
     console.log('servidor corriendo en http://localhost:3300');
 })
 /* app.get('/',(req,res)=>{
